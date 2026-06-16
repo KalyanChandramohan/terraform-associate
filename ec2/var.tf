eb602f119c4203c4bcde7aaef2f797c8e48db3ad
kalyanchandramohan <ragskalyan@gmail.com>
2026-06-16 14:45:37 +0530

    ec2 test

---
+++ ec2/var.tf
@@ -0,0 +1,28 @@
+variable "ami_id" {
+  description = "The ID of the AMI to use for the EC2 instance."
+  type        = string
+  default = "ami-0e38835daf6b8a2b9"
+}
+
+variable "instance_type" {
+  description = "The type of instance to create."
+  type        = string
+  default     = "t2.micro"
+  
+}
+
+variable "key_name" {
+  description = "The name of the key pair to use for the EC2 instance."
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnet in which to launch the EC2 instance."
+  type        = string
+}
+
+variable "instance_name" {
+  description = "The name to assign to the EC2 instance."
+  type        = string
+  default = "test"
+}
