14b639aa14f9cc439428a372da5817b460c2a58c
kalyanchandramohan <ragskalyan@gmail.com>
2026-06-16 14:49:20 +0530

    try

--- ec2/var.tf
+++ ec2/var.tf
@@ -14,11 +14,13 @@ variable "instance_type" {
 variable "key_name" {
   description = "The name of the key pair to use for the EC2 instance."
   type        = string
+  default = "ec2"
 }
 
 variable "subnet_id" {
   description = "The ID of the subnet in which to launch the EC2 instance."
   type        = string
+  default = "subnet-0c2202ea3c3532a98"
 }
 
 variable "instance_name" {
