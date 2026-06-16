e2e7ae3a6e4c460bf0a6500d064fa66e2a7d97d4
kalyanchandramohan <ragskalyan@gmail.com>
2026-06-16 14:52:12 +0530

    update ins type

--- ec2/var.tf
+++ ec2/var.tf
@@ -7,7 +7,7 @@ variable "ami_id" {
 variable "instance_type" {
   description = "The type of instance to create."
   type        = string
-  default     = "t2.micro"
+  default     = "t3.micro"
   
 }
 
